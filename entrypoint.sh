55aa5051752aee806ecf321318c1f4aefd0dabbc
vitrevance <64753385+vitrevance@users.noreply.github.com>
2024-04-03 17:42:51 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,6 +47,6 @@ else
   echo -n "${ss}" | qrencode -s 6 -o /wwwroot/${QR_Path}/vpn.png
 fi
 
-sudo ss-server -c /etc/shadowsocks-libev/config.json &
-rm -rf /etc/nginx/sites-enabled/default &
+ss-server -c /etc/shadowsocks-libev/config.json
+rm -rf /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
